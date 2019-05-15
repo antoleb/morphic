59d2054d101cc88b6618071cae7fe6e8274f0bac
antoleb <anleb96@gmail.com>
2019-05-15 15:26:17 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="morphic",
+    version="0.0.1",
+    description="python3 morphic",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/antoleb/morphic.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
+
